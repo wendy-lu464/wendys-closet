d1633c438b0356ec0e7e267fc67a93e9b6b3a9f9
wendy-lu464 <wendy.lu464@gmail.com>
2025-06-01 20:00:36 -0400

    Display "No results" on shop page when appropriate

--- frontend/src/pages/shop/ShopPage.jsx
+++ frontend/src/pages/shop/ShopPage.jsx
@@ -69,32 +69,39 @@ const ShopPage = () => {
                     />
 
                     {/* products */}
-                    <div>
-                        <h3 className='text-xl font-medium mb-4'>
-                            Showing {startProduct} to {endProduct} of {totalProducts} products</h3>
-                        <ProductCards products={products} />
+                    {
+                        products.length === 0 ? (<div><h3 className='text-xl font-medium mb-4'>No results</h3></div>) : (
+                            <div>
+                                <h3 className='text-xl font-medium mb-4'>
+                                    Showing {startProduct} to {endProduct} of {totalProducts} products</h3>
+                                <ProductCards products={products} />
 
-                        { /* pagination controls */}
-                        <div className='mt-6 flex justify-center'>
-                            <button
-                                disabled={currentPage === 1}
-                                onClick={() => handlePageChange(currentPage - 1)}
-                                className='px-4 py-2 bg-gray-300 text-gray-700 rounded-md mr-2'>Previous</button>
-                            {
-                                [...Array(totalPages)].map((_, index) => (
+                                { /* pagination controls */}
+                                <div className='mt-6 flex justify-center'>
                                     <button
-                                        key={index}
-                                        onClick={() => handlePageChange(index + 1)}
-                                        className={`px-4 py-2 ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} 
-                                   rounded-md mx-1`}>{index + 1}</button>
-                                ))
-                            }
-                            <button
-                                disabled={currentPage === totalPages}
-                                onClick={() => handlePageChange(currentPage + 1)}
-                                className='px-4 py-2 bg-gray-300 text-gray-700 rounded-md ml-2'>Next</button>
-                        </div>
-                    </div>
+                                        disabled={currentPage === 1}
+                                        onClick={() => handlePageChange(currentPage - 1)}
+                                        className='px-4 py-2 bg-gray-300 text-gray-700 rounded-md mr-2'>Previous</button>
+                                    {
+                                        [...Array(totalPages)].map((_, index) => (
+                                            <button
+                                                key={index}
+                                                onClick={() => handlePageChange(index + 1)}
+                                                className={`px-4 py-2 
+                                                    ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} 
+                                                    rounded-md mx-1`}>{index + 1}
+                                            </button>
+                                        ))
+                                    }
+                                    <button
+                                        disabled={currentPage === totalPages}
+                                        onClick={() => handlePageChange(currentPage + 1)}
+                                        className='px-4 py-2 bg-gray-300 text-gray-700 rounded-md ml-2'>Next</button>
+                                </div>
+
+                            </div>
+                        )
+                    }
                 </div>
             </section>
         </>
